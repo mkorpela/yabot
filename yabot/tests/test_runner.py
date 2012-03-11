08cd34445b36effd21853d5c5e15253b517b6f71
Mikko Korpela <mikko.korpela@reaktor.fi>
2012-03-11 18:37:19 +0200

    test stubs

---
+++ yabot/tests/test_runner.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class RunnerTestCase(unittest.TestCase):
+
+    def test_random_tests(self):
+        pass
+
+    def test_random_test_suites(self):
+        pass
+
+    def test_rerun_failed_tests(self):
+        pass
+
+    def test_rerun_failed_suites_when_random_suites(self):
+        pass
+
+    def test_all_failed_are_executed_even_if_there_are_more_of_them_then_the_required_fraction(self):
+        pass
+
+    def test_fraction_settings(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
